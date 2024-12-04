0d1caad58ff2983e613a7e27597ad5ad2b3bbf43
Shafa Alafghany <alafghanyshafa@gmail.com>
2024-12-04 14:29:49 +0700

    implement middleware

---
+++ user-service/middleware/jwt.go
@@ -0,0 +1,47 @@
+package middleware
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+)
+
+func JWTAuthInterceptor(secretKey string) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		fmt.Println(info.FullMethod)
+		if info.FullMethod == "/user.UserService/Register" || info.FullMethod == "/user.UserService/Login" {
+			return handler(ctx, req)
+		}
+
+		md, ok := metadata.FromIncomingContext(ctx)
+		if !ok {
+			return nil, status.Error(codes.Unauthenticated, "missing metadata")
+		}
+
+		authHeader, ok := md["authorization"]
+		if !ok || len(authHeader) == 0 {
+			return nil, status.Error(codes.Unauthenticated, "missing authorization header")
+		}
+
+		tokenStr := strings.TrimPrefix(authHeader[0], "Bearer ")
+		token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, errors.New("unexpected signing method")
+			}
+			return []byte(secretKey), nil
+		})
+
+		if err != nil || !token.Valid {
+			return nil, status.Error(codes.Unauthenticated, "invalid token")
+		}
+
+		return handler(ctx, req)
+	}
+}
